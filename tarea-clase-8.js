b2f40e422a0a4270879be9424bd49846e431f957
Santino-Gonzalez <96146946+Santino-Gonzalez@users.noreply.github.com>
2024-10-12 20:33:02 -0300

    Crea validación de integrantes
    Se crea la función que valida que el usuario ingrese correctamente la cantidad de integrantes.

--- tarea-clase-8.js
+++ tarea-clase-8.js
@@ -6,4 +6,12 @@ necesarias.
 TIP: Las edades no pueden tener decimales.
 */
 
-
+function validarCantidadIntegrantes(cantidad){
+    if(cantidad === 0){
+        console.error("La cantidad de integrantes debe ser de al menos 1.");
+    }else if(cantidad < 0){
+        console.error("La cantidad de integrantes no puede ser negativa.");
+    }else{
+        console.log("Se ha verificado correctamente la cantidad de integrantes.");
+    }
+}
